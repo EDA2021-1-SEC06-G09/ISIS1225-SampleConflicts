07c0afb627c53ae78ea1526358be85dc135dc3fd
Almanza33 <d.almanza@uniandes.edu.co>
2021-02-07 20:33:07 -0500

    cambios del estudiante 2

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags")
     print("0- Salir")
 
 
@@ -64,6 +65,7 @@ def loadBookTags():
     Cargar los Tags de libros
     """
     # TO-DO: Modificación de Est-1 en el Lab 2
+    
 
 
 """
@@ -81,14 +83,19 @@ while True:
 
 
         # TO-DO: Modificación de Est-2 en el Lab 2
+        print("Ultimo libro cargado " + str(lt.lastElement(books)))
+
+
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
+    elif int(inputs[0]) == 3:
         # TO-DO: Modificación de Est-2 en el Lab 2
-        pass
+        print("Cargando informacion de Book-Tags")
+        booktags = loadBookTags()
+        print("Total de Book-tags cargados " + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
